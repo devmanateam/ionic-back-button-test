003e794e91b3724a74aacd2c705fb985c2a46bba
devmanagalaxy <devmanateam@outlook.com>
2021-10-01 22:39:55 +0800

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor3-backbutton-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
